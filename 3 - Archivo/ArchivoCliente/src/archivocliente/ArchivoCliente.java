3157d0fcbea72fbe8889c1484f8597bb009f396a
Alexander Gámez Urías <48527916+Darkceus@users.noreply.github.com>
2019-09-15 19:15:17 -0600

    Subí el Cliente-Servidor de Archivos

---
+++ 3 - Archivo/ArchivoCliente/src/archivocliente/ArchivoCliente.java
@@ -0,0 +1,11 @@
+package archivocliente;
+
+import cliente.Cliente;
+
+public class ArchivoCliente {
+    
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(args);
+        cliente.iniciarClienteArchivo();
+    }
+}
